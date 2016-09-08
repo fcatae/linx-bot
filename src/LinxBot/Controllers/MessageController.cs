98e750c92f6269cd4b8f1856eb2831b5f7143dc9
Fabricio Catae <fcatae@microsoft.com>
2016-09-08 09:05:54 -0300

    add BotFramework

---
+++ src/LinxBot/Controllers/MessageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Connector;
+
+namespace LinxBot.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {
+        [HttpPost]
+        public void Post([FromBody] Activity activity)
+        {
+            if(activity.Type == ActivityTypes.Message)
+            {
+                var client = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                var message = activity.CreateReply("Hello");
+
+                client.Conversations.ReplyToActivity(message);
+            }
+        }
+    }
+}
